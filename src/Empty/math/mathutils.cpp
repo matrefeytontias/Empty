02d2c00da51438fa1eb2f18530ee960718b293ce
matrefeytontias <mattias@refeyton.fr>
2020-07-13 20:33:23 +0200

    Relocate mathutil functions

---
+++ src/Empty/math/mathutils.cpp
@@ -0,0 +1,54 @@
+#include "Empty/math/funcs.h"
+#include "Empty/math/mathutils.hpp"
+
+auto math::rotateX(float a) -> mat4
+{
+	mat4 r = mat4::Identity();
+	float c = cosf(a), s = sinf(a);
+	r(1, 1) = r(2, 2) = c;
+	r(1, 2) = -(r(2, 1) = s);
+	return r;
+}
+
+auto math::rotateY(float a) -> mat4
+{
+	mat4 r = mat4::Identity();
+	float c = cosf(a), s = sinf(a);
+	r(0, 0) = r(2, 2) = c;
+	r(2, 0) = -(r(0, 2) = s);
+	return r;
+}
+
+auto math::rotateZ(float a) -> mat4
+{
+	mat4 r = mat4::Identity();
+	float c = cosf(a), s = sinf(a);
+	r(0, 0) = r(1, 1) = c;
+	r(0, 1) = -(r(1, 0) = s);
+	return r;
+}
+
+auto math::lookAt(vec3 forward, vec3 up) -> mat4
+{
+	vec3 x = normalize(cross(up, forward)),
+		y = normalize(cross(forward, x));
+	mat4 r;
+	r.column(0) = vec4(x, 0);
+	r.column(1) = vec4(y, 0);
+	r.column(2) = vec4(forward, 0);
+	r.column(3) = vec4(0, 0, 0, 1);
+	return r;
+}
+
+auto math::lookAt(vec3 origin, vec3 target, vec3 up) -> mat4
+{
+	vec3 z = normalize(target - origin),
+		x = normalize(cross(up, z)),
+		y = normalize(cross(z, x));
+	mat4 r;
+	r.column(0) = vec4(x, 0);
+	r.column(1) = vec4(y, 0);
+	r.column(2) = vec4(z, 0);
+	r.column(3) = vec4(0, 0, 0, 1);
+	return r;
+}
---
+++ src/Empty/math/mathutils.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Empty/math/mat.h"
+
+/**
+ * Various mathematical utility functions.
+ */
+
+namespace math
+{
+	/**
+	 * Returns a matrix representing a rotation of a given angle around the X axis.
+	 */
+	mat4 rotateX(float a);
+
+	/**
+	 * Returns a matrix representing a rotation of a given angle around the Y axis.
+	 */
+	mat4 rotateY(float a);
+
+	/**
+	 * Returns a matrix representing a rotation of a given angle around the Z axis.
+	 */
+	mat4 rotateZ(float a);
+
+	/**
+	 * Returns a matrix that transforms the Z axis into a given vector. This
+	 * makes it so that transforming a vector with this matrix makes it "look"
+	 * in a direction.
+	 */
+	mat4 lookAt(vec3 forward, vec3 up);
+
+	/**
+	 * Returns a matrix that makes the Z axis look at a given target. This
+	 * makes it so that transforming a vector with this matrix makes it "look at"
+	 * a target.
+	 */
+	mat4 lookAt(vec3 origin, vec3 target, vec3 up);
+}
